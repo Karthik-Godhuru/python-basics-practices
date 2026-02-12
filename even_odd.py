7bd98b525de40a598936f3a60dce8b25f8fdc669
KARTHIK GODHURU <karthikdevanga001@gmail.com>
2026-02-12 23:14:17 +0530

    Update even_odd.py

--- even_odd.py
+++ even_odd.py
@@ -1,8 +1,9 @@
 #even_odd program
 #Day-3 pratice
 #Author:Karthik Godhuru 
-num=int(input("enter the number:"))
-if num % 2==0:
-	print("the number is even")
-else:
-	print("the number is odd")
+num=int(input('enter a number: '))
+for i in range ( 1,num):
+    if i % 2==0:
+     	print(i,"even")
+    else:
+     	print(i,"odd")
