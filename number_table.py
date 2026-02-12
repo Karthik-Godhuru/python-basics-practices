58ad3efdb574bb19f72d54d28865eafba3ccafe2
KARTHIK GODHURU <karthikdevanga001@gmail.com>
2026-02-12 23:23:18 +0530

    Update number_table.py

--- number_table.py
+++ number_table.py
@@ -1,7 +1,9 @@
 # Number Table Program
 # Day 2 Practice
-# Author: Karthik Godhuru
+# Author: Karthik Godhuru 
 
-num=int(input("enter the number:"))
-for i in range (1,11):
-	print(num,"x",i,"=",num*i)
+for num in range(1,21):
+	print()
+	print("table of",num)
+	for i in range(1,11):
+		print(num,"x",i,"=",num*i)
