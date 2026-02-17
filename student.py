af01f6a375b9cab7dae2d2e4eb096956174899e2
KARTHIK GODHURU <karthikdevanga001@gmail.com>
2026-02-17 22:59:16 +0530

    student-marks-analyzer
    A Python program that analyzes student marks (total, average, highest, lowest, count above 75)

---
+++ student.py
@@ -0,0 +1,28 @@
+marks = [75,80,85,90,95]
+
+total = 0
+
+for mark in marks:
+    total = total + mark
+    print("Current total:", total)
+print("Final total:", total)
+average=total/len(marks)
+print("average:",round(average,2))
+highest=marks[0]
+for mark in marks:
+	if mark > highest:
+		highest = mark
+print(highest)
+lowest=marks[0]
+for mark in marks:
+	if mark < lowest:
+		lowest = mark
+print(lowest)
+count_above_75 = 0
+
+for mark in marks:
+    if mark > 75:
+        count_above_75 = count_above_75 + 1
+
+print(count_above_75)
+
