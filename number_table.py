199d25b5c0f80a82204b23025eae9d154806437d
KARTHIK GODHURU <karthikdevanga001@gmail.com>
2026-02-18 23:39:43 +0530

    Update number_table.py

--- number_table.py
+++ number_table.py
@@ -1,9 +1,17 @@
-# Number Table Program
-# Day 2 Practice
-# Author: Karthik Godhuru 
-
-for num in range(1,21):
-	print()
-	print("table of",num)
-	for i in range(1,11):
-		print(num,"x",i,"=",num*i)
+for num in range(1, 21):
+    # Outer loop runs from 1 to 20
+    # 'num' represents the number whose table we are printing
+    
+    print()
+    # Print a blank line for better formatting between tables
+    
+    print("Table of", num)
+    # Print the heading for the current multiplication table
+    
+    for i in range(1, 11):
+        # Inner loop runs from 1 to 10
+        # 'i' represents the multiplier
+        
+        print(num, "x", i, "=", num * i)
+        # Print the multiplication result
+        # Example: 5 x 3 = 15
