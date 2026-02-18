5188371f8ec31d5a9a7697df52e4f77daa1e62bf
KARTHIK GODHURU <karthikdevanga001@gmail.com>
2026-02-18 23:40:54 +0530

    Update even_odd.py

--- even_odd.py
+++ even_odd.py
@@ -1,9 +1,20 @@
-#even_odd program
-#Day-3 pratice
-#Author:Karthik Godhuru 
-num=int(input('enter a number: '))
-for i in range ( 1,num):
-    if i % 2==0:
-     	print(i,"even")
+
+
+num = int(input('Enter a number: '))
+# Take integer input from the user.
+# This number decides how many values we check.
+
+for i in range(1, num):
+    # Loop from 1 up to (but not including) num.
+    # Example: if num = 6 → values will be 1,2,3,4,5
+
+    if i % 2 == 0:
+        # % is modulus operator.
+        # i % 2 checks remainder when divided by 2.
+        # If remainder is 0 → number is even.
+
+        print(i, "even")
     else:
-     	print(i,"odd")
+        # If remainder is not 0 → number is odd.
+
+        print(i, "odd")
